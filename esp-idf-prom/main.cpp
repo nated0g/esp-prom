d0690ceec331cd90c0b6cc5067d549ac149dd559
nated0g <n.usher87@gmail.com>
2022-04-03 23:02:01 -0700

    initial commit, working

---
+++ esp-idf-prom/main.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/unistd.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <string.h>
+#include "esp_err.h"
+#include "esp_log.h"
+#include "prom.h"
+
+
+static const char *TAG = "example";
+
+
+void app_main(void)
+{
+    ESP_LOGI(TAG, "First Tryyyyy!");
+}
