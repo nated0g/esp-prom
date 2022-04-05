23eda19d82f2f7d9cbcc8c8b54cca52c44bd4861
nated0g <n.usher87@gmail.com>
2022-04-05 12:33:35 -0700

    refactor, add metrics task heap monitor

--- components/esp-prom-httpd/default-metrics.c
+++ components/esp-prom-httpd/default-metrics.c
@@ -14,6 +14,9 @@ void default_metrics_init(void) {
   min_free_heap_gauge = prom_collector_registry_must_register_metric(
     prom_gauge_new("esp_min_free_heap_size", "minimum free heap counted during execution", 0, NULL));
 
+  task_min_free_stack_gauge = prom_collector_registry_must_register_metric(
+    prom_gauge_new("esp_task_min_free_stack_size", "minimum free stack for default metrics task", 0, NULL));
+
   xTaskCreate(get_default_metrics_task,
               "prom_default_metrics",
               4096, 
@@ -27,6 +30,7 @@ void get_default_metrics_task(void)
   while (1) {
     prom_gauge_set(free_heap_gauge, esp_get_free_heap_size(), NULL);
     prom_gauge_set(min_free_heap_gauge, esp_get_minimum_free_heap_size(), NULL);
+    prom_gauge_set(task_min_free_stack_gauge, uxTaskGetStackHighWaterMark(NULL), NULL);
     vTaskDelay(pdMS_TO_TICKS(1000));
   }
 }
--- components/esp-prom-httpd/default-metrics.h
+++ components/esp-prom-httpd/default-metrics.h
@@ -5,6 +5,7 @@
 prom_counter_t *request_counter;
 prom_gauge_t *free_heap_gauge;
 prom_gauge_t *min_free_heap_gauge;
+prom_gauge_t *task_min_free_stack_gauge;
 
 void default_metrics_init(void);
 
